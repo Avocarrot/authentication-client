4edc179db98d9b373322ba1979005e891d655830
emiliosnic <nicolaouemilios@gmail.com>
2017-01-10 15:35:33 +0200

    fix(authentication-clients) Adds missing client argument to setup

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const Consumer = require('./utils/consumer');
 
 var AuthenticationClient = (function() {
   let client = new Client('id', 'secret');
-  let consumer = new Consumer('http://localhost:3013', 'https://login.avocarrot.com', client);
+  let consumer = new Consumer(client, 'http://localhost:3013', 'https://login.avocarrot.com');
   let store = new Store('avocarrot');
 
   return new Authenticator(store, consumer);
