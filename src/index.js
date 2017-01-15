d5aee4bc3ffe094b70374066c0ba388aa3e00101
emiliosnic <nicolaouemilios@gmail.com>
2017-01-15 23:45:34 +0200

    chore(branch) Rebases with remote

--- src/index.js
+++ src/index.js
@@ -7,16 +7,35 @@ const Client = require('./models/client');
 const Consumer = require('./services/consumer');
 
 /**
- * AuthenticationClient
- * @namespace
+ * @namespace AuthenticationClient
  */
-var AuthenticationClient = (function() {
+const AuthenticationClient = (function() {
 
   /**
    * Store instance
    * @private
    */
   const store = new Store(config.store.namespace);
+
+  /**
+   * Cached instances
+   * @private
+   */
+  const instances = new Map();
+
+  /**
+   * Generates an AuthenticationClient instance
+   * @private
+   * @param {String} client_id - The client_id to set
+   * @param {String} client_secret - The client_secret
+   * @return {Authenticator}
+   */
+  const generateInstance = function(client_id, client_secret){
+    const client = new Client(client_id, client_secret);
+    const consumer =  new Consumer(client, config.host.endpoint, config.host.login_url);
+    const user = new User(store, consumer);
+    return new Authenticator(user, consumer);
+  }
 
   return {
     /**
@@ -27,12 +46,15 @@ var AuthenticationClient = (function() {
      * @param {String} client_secret - The Client secret
      */
     getInstanceFor(client_id, client_secret) {
-      const consumer =  new Consumer(new Client(client_id, client_secret), config.host.endpoint, config.host.login_url);
-      const user = new User(store, consumer);
-      const authenticator = new Authenticator(user, consumer);
-      return authenticator;
+      const key = `${client_id}-${client_secret}`;
+      if (instances.has(key)){
+        return instances.get(key);
+      }
+      let instance = generateInstance(...arguments);
+      instances.set(key, instance);
+      return instance;
     }
   }
 })();
 
-global.window.AuthenticationClient = AuthenticationClient;
+module.exports = AuthenticationClient;
