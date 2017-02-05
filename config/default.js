c60a7a2e182f964ef7c47af13c87047b86a6830a
emiliosnic <nicolaouemilios@gmail.com>
2017-02-05 17:46:40 +0200

    fix(StorageClient) Updates iframeHub URL

--- config/default.js
+++ config/default.js
@@ -7,6 +7,6 @@ module.exports = {
   },
   store: {
     domain: 'avocarrot',
-    iframeHub: 'https://login.avocarrot.com/hub.html',
+    iframeHub: 'https://s3.amazonaws.com/login.avocarrot.com/hub/index.html',
   },
 };
