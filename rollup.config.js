c8c393963fadbdee3a15dc11dc5b57f4b8b87cba
Victor Castro <victor.castro@glispamedia.com>
2017-05-19 16:19:07 +0200

    New build

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,13 @@
+import json from 'rollup-plugin-json';
+import resolve from 'rollup-plugin-node-resolve';
+import babel from 'rollup-plugin-babel';
+import commonjs from 'rollup-plugin-commonjs';
+
 export default {
   entry: 'src/index.js',
   format: 'cjs',
-  dest: 'bundle.js',
+  plugins: [json(), resolve(), commonjs(), babel({
+    exclude: 'node_modules/**',
+  })],
+  dest: 'dist/bundle.js',
 };
